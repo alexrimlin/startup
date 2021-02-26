12eae736f2a214d328de636861eab883cbac5f2b
Alex Rimlin <alexander.rimlin@protonmail.com>
2021-02-26 18:48:59 +0200

    fix

--- template/remconsole.sh
+++ template/remconsole.sh
@@ -2,6 +2,6 @@
 command = "$@"
 if [ "$1" != "restart" ]
 then
-    ssh -i ${spdir}/main_key.pem -p 443 ubuntu@13.48.90.95 "sudo screen -S spigot -X stuff \"$@\n\""
+    ssh -i $(pwd)/main_key.pem -p 443 ubuntu@13.48.90.95 "sudo screen -S spigot -X stuff \"$@\n\""
 else
-    ssh -i ${spdir}/main_key.pem -p 443 ubuntu@13.48.90.95 "sh ${spdir}/restart.sh"
+    ssh -i $(pwd)/main_key.pem -p 443 ubuntu@13.48.90.95 "sh $(pwd)/restart.sh"
