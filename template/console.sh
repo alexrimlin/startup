029d3b8162f8c6fcc1fe61055c57e8804d5d8e8d
Alex Rimlin <alexander.rimlin@protonmail.com>
2021-02-26 13:08:23 +0200

    Added restart functionality

--- template/console.sh
+++ template/console.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ "$1" == "start" ] && [ "$(screen -ls | grep -o 'spigot')" != 'spigot' ]
 then
-    screen -dmS spigot
-    sleep 0.5
+    screen -dmS spigot -L -Logfile screen.log
+    echo "Starting spigot..."
     screen -S spigot -X stuff "sh serve.sh\n"
+    echo $(tail -f -n 1 screen.log | grep -m 1 ": Done")
 
 elif [ "$1" == "kill" ]
 then
@@ -11,10 +12,17 @@ then
     then
         screen -S spigot -X stuff "stop\n"
         echo "Stopping spigot and killing the screen... (approx. 10sec)"
-        sleep 10
+        echo $(tail -f -n 1 screen.log | grep -m 1 "DIM1")
     fi
-    screen -X -S spigot kill\
+    screen -X -S spigot kill
+elif [ "$1" == "restart" ]
+then
+    screen -S spigot -X stuff "stop\n"
+    echo "Stopping spigot..."
+    echo $(tail -f -n 1 screen.log | grep -m 1 "DIM1")
+    echo "Starting spigot..."
+    screen -S spigot -X stuff "sh serve.sh\n"
+    echo $(tail -f -n 1 screen.log | grep -m 1 ": Done")
 else
     screen -r spigot
 fi
-
